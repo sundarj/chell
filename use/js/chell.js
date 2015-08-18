24bec8abd49e9084bd2e6b652e445f8a5c26c940
Sundar Joshi <sundar.joshi@contentive.com>
2015-08-18 12:20:49 +0100

    i/o functions, previous inputs stop being contentEditable

--- use/js/chell.js
+++ use/js/chell.js
@@ -20,29 +20,46 @@
 	function focus(elt) {
 		var sel = window.getSelection();
 		var range = document.createRange();
-		range.setStartAfter(elt.firstElementChild);
+		range.setStartAfter(elt);
 		range.collapse(true);
 		sel.removeAllRanges();
 		sel.addRange(range);
-		elt.removeChild(elt.firstElementChild);
 	}
 	
 	Chell.prototype.newline = function (type) {
 		var self = this.terminal.appendChild(create('div', {
-			className: 'line ' + type,
-			contentEditable: true
+			className: 'am ' + type,
+			contentEditable: (type === 'input')
 		}));
 		if (type === 'input') {
-			self.appendChild(document.createElement('span'));
 			self.focus();
-			focus(self);
+			focus(self.appendChild(document.createTextNode("\u200B")));
 		}
+		return self;
 	}
 	
 	Chell.prototype.exec = function (command) {
 		command = command.split('.');
 		arguments = [].slice.call(arguments, 1);
 		chrome[command[0]][command[1]].apply(chrome, arguments);
+	}
+	
+	Chell.prototype.output = function (command) {
+		var self = this;
+		var output = this.newline('output');
+		output.innerHTML = 'doing something...';
+		setTimeout(function () {
+			output.innerHTML += '<br>done';
+			self.input();
+		}, 1000);
+	}
+	
+	Chell.prototype.input = function () {
+		[].forEach.call(this.terminal.querySelectorAll('.input'), function (line) {
+			console.log(line);
+			line.contentEditable = false;
+		});
+		this.newline('input');
 	}
 	
 	Chell.prototype.start = function () {
@@ -70,7 +87,7 @@
 	}
 	
 	function trim(str) { 
-		return str.replace(/^\s+|\s+$/g, '');
+		return str.replace(/^\s+|\s+$/g, '').replace(/\u200B/g, '');
 	};
 	
 	var chell = window.Chell = new Chell;
@@ -80,7 +97,9 @@
 		var self = e.target;
 
 		if (!trim(self.textContent).length) {
-			chell.newline('input');
+			chell.input();
+		} else {
+			chell.output(self.textContent);
 		}
 	
 	});
